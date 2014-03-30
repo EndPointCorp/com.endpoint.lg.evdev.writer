8593a88ca494e49d905dffa7216dc02eafa4f5c9
Matt Vollrath <matt@endpoint.com>
2014-03-30 14:34:28 -0700

    Use default java for evdev.writer lib build

--- src/main/native/build-libs.sh
+++ src/main/native/build-libs.sh
@@ -3,7 +3,7 @@
 set -e
 
 export CLASSPATH="../../../build/classes/main:../../../../com.endpoint.lg.support/build/classes/main"
-JAVA_PATH=/usr/lib/jvm/java-7-openjdk-amd64
+JAVA_PATH=/usr/lib/jvm/default-java
 
 ${JAVA_PATH}/bin/javah com.endpoint.lg.evdev.writer.UinputDevice
 g++ -fPIC -o libispaces-uinput.so -lc -shared -I${JAVA_PATH}/include -I${JAVA_PATH}/include/linux ispaces-uinput.cpp

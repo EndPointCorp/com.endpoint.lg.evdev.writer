2ecf97292065d9953908849f5a54963920ea061b
Wojciech Ziniewicz <wojciech.ziniewicz@gmail.com>
2015-05-26 16:24:35 +0200

    Better uinput device handling

--- src/main/native/build-libs.sh
+++ src/main/native/build-libs.sh
@@ -3,7 +3,11 @@
 set -e
 
 export CLASSPATH="../../../build/classes/main:../../../../com.endpoint.lg.support/build/classes/main"
-JAVA_PATH=/usr/lib/jvm/default-java
 
-${JAVA_PATH}/bin/javah com.endpoint.lg.evdev.writer.UinputDevice
-g++ -fPIC -o libispaces-uinput.so -lc -shared -I${JAVA_PATH}/include -I${JAVA_PATH}/include/linux ispaces-uinput.cpp
+if [ -z ${JAVA_PATH} ]; then
+  echo "JAVA_PATH must be set to e.g. /usr/lib/jvm/java-1.7.0-openjdk-amd64/"
+  exit 1
+else
+  ${JAVA_PATH}/bin/javah com.endpoint.lg.evdev.writer.UinputDevice
+  g++ -fPIC -o libispaces-uinput.so -lc -shared -I${JAVA_PATH}/include -I${JAVA_PATH}/include/linux ispaces-uinput.cpp
+fi
